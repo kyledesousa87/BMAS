c49dbd7e184104ee4bdd2485c592d6098bf0bbe8
Kyle De Sousa <kylesfolio@gmail.com>
2018-09-23 21:37:14 +0100

    reveal test

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
+
 // Static Server + watching scss/html files
 gulp.task('serve', function() {
 
